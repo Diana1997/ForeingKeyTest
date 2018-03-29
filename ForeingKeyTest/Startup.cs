cc2f773be9ea5f73cb5131e5ec8cd4fb75414b9e
Diana <Diana@DESKTOP-RLN4LA8>
2018-03-29 11:49:34 +0400

    Add project files.

---
+++ ForeingKeyTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForeingKeyTest.Startup))]
+namespace ForeingKeyTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
